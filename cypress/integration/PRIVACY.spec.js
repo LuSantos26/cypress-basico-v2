cc3d4b92b3c11577800e82ed056bae1144db86ab
Luana Santos <luana.ferreirasantos@everis.nttdata.com>
2023-07-09 19:15:16 -0300

    Implementação do Cypress-Lodash, para loop mantendo o clear code

--- cypress/integration/PRIVACY.spec.js
+++ cypress/integration/PRIVACY.spec.js
@@ -1,4 +1,9 @@
-it('Testa a página a política de privacidade de forma independente', function(){
+	
+Cypress._.times(5,function(){
+	it('Testa a página a política de privacidade de forma independente', function() {
 		cy.visit('./src/PRIVACY.html')
+		
 		cy.contains('Talking About Testing').should('be.visible')
 	})
+	
+})
